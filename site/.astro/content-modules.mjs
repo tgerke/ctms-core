2873b7e4ec3e219314d9dcdf3cf2abf37a3c5f83
Travis Gerke <tgerke@mail.harvard.edu>
2026-07-23 10:56:23 -0700

    Untrack site/.astro, add site/.gitignore for Astro build outputs

--- site/.astro/content-modules.mjs
+++
@@ -1,6 +0,0 @@
-
-export default new Map([
-["src/content/docs/data-model.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdata-model.mdx&astroContentModuleFlag=true")],
-["src/content/docs/getting-started.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started.mdx&astroContentModuleFlag=true")],
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")]]);
-		
